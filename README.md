1145ca516c894b26b76ab821ad30e7ae66476691
Samuel Chen <tingchen776@gmail.com>
2023-11-01 23:41:49 -0700

    Initial Commit with Main App Screen

---
+++ README.md
@@ -0,0 +1,3 @@
+# **Pantry Pal**
+---
+An app to allow users to keep track of their recipes using the ingredients they have on hand. 
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("My Application");
+
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
