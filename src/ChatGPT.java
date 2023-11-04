e42a6af4a90fdc7bb9c95146c74830eca95543c7
tron-e <93411591+tron-e@users.noreply.github.com>
2023-11-04 14:10:13 -0700

    generate recipe from chatGPT

---
+++ src/ChatGPT.java
@@ -0,0 +1,76 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+public class ChatGPT{
+
+    private static final String MODEL = "text-davinci-003"; // Replace with your model
+    private static final String API_KEY = "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"; // Replace with your API key
+    private static final String API_ENDPOINT = "https://api.openai.com/v1/completions";
+
+    public static void main(String[] args) throws Exception {
+        String mealType = "dinner";
+        String ingredients = "Chicken, rice, eggs";
+
+        String recipe = generateRecipe(mealType, ingredients);
+
+        saveRecipe("recipe.txt", recipe);
+    }
+
+    private static String generateRecipe(String mealType, String ingredients) throws Exception {
+        String prompt = "Give me a concise recipe for a " +
+                mealType +
+                " meal that ONLY uses the following ingredients: " +
+                ingredients;
+        int maxTokens = 250;
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("model", MODEL);
+        requestBody.put("prompt", prompt);
+        requestBody.put("max_tokens", maxTokens);
+        requestBody.put("temperature", 1.0);
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest
+        .newBuilder()
+        .uri(new URI(API_ENDPOINT))
+        .header("Content-Type", "application/json")
+        .header("Authorization", String.format("Bearer %s", API_KEY))
+        .POST(HttpRequest.BodyPublishers.ofString(requestBody.toString()))
+        .build();
+
+        HttpResponse<String> response = client.send(
+        request,
+        HttpResponse.BodyHandlers.ofString()
+        );
+
+        String responseBody = response.body();
+
+        JSONObject responseJSON = new JSONObject(responseBody);
+        JSONArray choices = responseJSON.getJSONArray("choices");
+        String recipeGPT = choices.getJSONObject(0).getString("text");
+        
+        return recipeGPT;
+    }
+
+    private static void saveRecipe(String fp, String recipe) {
+        try {
+            FileWriter fw = new FileWriter(fp);
+            fw.write(recipe);
+            fw.close();
+            System.out.println("Recipe saved to " + fp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
