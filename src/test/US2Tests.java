48cbf6b5fb6ebe840cb72a3d43084f419dcf5bb9
Samuel Chen <tingchen776@gmail.com>
2023-11-11 16:30:52 -0800

    Add skeleton code for US2 testing

--- src/test/US2Tests.java
+++ src/test/US2Tests.java
@@ -1,9 +1,19 @@
 package test;
 
+import org.junit.jupiter.api.Test;
 
 /*
  * A class to test tasks related to US2.
+ * Specifically aimed at testing saving recipes into db.
  */
 public class US2Tests {
-    
+    @Test
+    void testSaveRecipe() {
+        
+    }
+
+    @Test
+    void testConnectToDB() {
+
+    }
 }
