709db855f3cf7d64f0c019b4a88a2135f8cf09ea
aaruu03 <berryaarya@gmail.com>
2023-11-12 19:02:22 -0800

    Tests for Deletion

---
+++ src/test/java/US2Tests.java
@@ -0,0 +1,22 @@
+
+
+import org.junit.jupiter.api.Test;
+
+import main.java.server.database;
+
+/*
+ * A class to test tasks related to US2.
+ * Specifically aimed at testing saving recipes into db.
+ */
+public class US2Tests {
+    @Test
+    void testSaveRecipe() {
+        database db = new database();
+        db.addFileToDb();
+    }
+
+    @Test
+    void testConnectToDB() {
+
+    }
+}
