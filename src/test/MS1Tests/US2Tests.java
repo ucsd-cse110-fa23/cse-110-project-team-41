3919b036b1a2402e5e9dd27263f9ca8ee3137d06
Sking56 <tingchen776@gmail.com>
2023-12-03 00:21:54 -0800

    Clean gradle to reset files and refactor tests and fix tests and finish US1 tests

---
+++ src/test/MS1Tests/US2Tests.java
@@ -0,0 +1,55 @@
+package test.MS1Tests;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.server.database;
+
+/*
+ * A class to test tasks related to US2.
+ * Specifically aimed at testing saving recipes into db.
+ */
+public class US2Tests {
+    @Test
+    void testRecipeFileExists() {
+        File recipe = new File("src/main/java/recipe.txt");
+        assert(recipe.exists());
+    }
+
+    @Test
+    void testConnectToDB() {
+        database db = new database();
+        assert(db != null);
+    }
+
+    @Test
+    void testFileReading() {
+        database db = new database();
+        File tempFile = new File("src/main/temp.txt");
+        try{
+            tempFile.createNewFile();
+            FileWriter writer = new FileWriter(tempFile);
+            writer.write("Test Recipe\n");
+            writer.write("Ingredients:\n Chicken\n Rice\n Eggs");
+            writer.write("Instructions:\n Cook Chicken\n Cook Rice\n Cook Eggs");
+            writer.close();
+            List<String> recipe = db.processFile(tempFile.toPath().toString());
+            assert(recipe.size() == 3);
+            assert(recipe.get(0).length() > 0);
+            assert(recipe.get(1).length() > 0);
+            assert(recipe.get(2).length() > 0);
+            tempFile.delete();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
